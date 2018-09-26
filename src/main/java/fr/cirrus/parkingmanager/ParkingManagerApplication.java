eb2bdd9b0dd1017d39e9a846e2d69cdd2e3afbaf
Ludovic Chane Won In <lchanewonin@cirrus-info.com>
2018-09-26 17:10:35 +0400

    First commit

---
+++ src/main/java/fr/cirrus/parkingmanager/ParkingManagerApplication.java
@@ -0,0 +1,12 @@
+package fr.cirrus.parkingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/cirrus/parkingmanager/ParkingManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.cirrus.parkingmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
