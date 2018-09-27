f5d0914954d32e0f47086b2001d514f09f675aa2
Ludovic Chane Won In <lchanewonin@cirrus-info.com>
2018-09-27 11:16:11 +0400

    Commit for 2-Database

--- src/main/java/fr/cirrus/parkingmanager/models/ParkingPlace.java
+++ src/main/java/fr/cirrus/parkingmanager/models/ParkingPlace.java
@@ -1,10 +1,18 @@
 package fr.cirrus.parkingmanager.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "parkingplace")
 public class ParkingPlace {
+    @Id
     private String numero;
 
     public ParkingPlace(String numero) {
         this.numero = numero;
+    }
+
+    public ParkingPlace() {
     }
 
     public String getNumero() {
