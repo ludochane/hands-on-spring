7ea0ba954e3396599f296b78d76070f124789918
Ludovic Chane Won In <lchanewonin@cirrus-info.com>
2018-09-27 11:16:11 +0400

    Commit for 2-Database

--- src/main/java/fr/cirrus/parkingmanager/controllers/ParkingPlaceController.java
+++ src/main/java/fr/cirrus/parkingmanager/controllers/ParkingPlaceController.java
@@ -1,28 +1,33 @@
 package fr.cirrus.parkingmanager.controllers;
 
 import fr.cirrus.parkingmanager.models.ParkingPlace;
+import fr.cirrus.parkingmanager.repositories.ParkingPlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/parkingPlaces")
 public class ParkingPlaceController {
 
+    private final ParkingPlaceRepository repository;
+
+    @Autowired
+    public ParkingPlaceController(ParkingPlaceRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("")
-    public List<ParkingPlace> all() {
-        return Arrays.asList(
-                new ParkingPlace("1"),
-                new ParkingPlace("2")
-        );
+    public Iterable<ParkingPlace> all() {
+        return this.repository.findAll();
     }
 
     @GetMapping("{numero}")
-    public ParkingPlace get(@PathVariable("numero") String numero) {
-        return new ParkingPlace(numero);
+    public Optional<ParkingPlace> get(@PathVariable("numero") String numero) {
+        return this.repository.findById(numero);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into PARKINGPLACE (numero) values (1);
+insert into PARKINGPLACE (numero) values (2);
+insert into PARKINGPLACE (numero) values (3);
