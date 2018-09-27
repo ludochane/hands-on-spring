f3a3b55eb58fb5e776f46d0567d02825543314bd
Ludovic Chane Won In <lchanewonin@cirrus-info.com>
2018-09-27 09:45:33 +0400

    Refactor ParkingPlaceController
    Mutualise "/parkingPlaces"

--- src/main/java/fr/cirrus/parkingmanager/controllers/ParkingPlaceController.java
+++ src/main/java/fr/cirrus/parkingmanager/controllers/ParkingPlaceController.java
@@ -2,19 +2,27 @@ package fr.cirrus.parkingmanager.controllers;
 
 import fr.cirrus.parkingmanager.models.ParkingPlace;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/parkingPlaces")
 public class ParkingPlaceController {
 
-    @GetMapping("parkingPlaces/all")
+    @GetMapping("")
     public List<ParkingPlace> all() {
         return Arrays.asList(
                 new ParkingPlace("1"),
                 new ParkingPlace("2")
         );
+    }
+
+    @GetMapping("{numero}")
+    public ParkingPlace get(@PathVariable("numero") String numero) {
+        return new ParkingPlace(numero);
     }
 }
